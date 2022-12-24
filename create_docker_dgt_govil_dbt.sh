cfc9c12547a1e8182a244469041cdfa7ef8ba9e4
LAPTOP-HAHEDJOL\Gil <gilc86@gmail.com>
2022-12-24 16:02:56 +0200

    add user name

--- create_docker_dgt_govil_dbt.sh
+++ create_docker_dgt_govil_dbt.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 export Dbt_project_Name=dgt_govil_dbt
+export userName=$(gcloud config list account --format "value(core.account)")
+export userName=$(cut -d "@" -f1 <<< "$userName")
+echo $userName
+DIRECTORY_REPO='govil_airflow_k8_dbt'
 
 echo create docker for $Dbt_project_Name
 echo "creator: Gil Kal"
 
-cd /home/gilc/projects/
+cd /home/$userName/projects/
 
 # Check the $DIRECTORY_REPO is exists and delete
-DIRECTORY_REPO='govil_airflow_k8_dbt'
+
 if [ -d "$DIRECTORY_REPO" ]; then
   echo "$DIRECTORY_REPO does exist."
   rm -rf $DIRECTORY_REPO
@@ -19,7 +23,7 @@ git clone https://github.com/gilc86/$DIRECTORY_REPO.git
 echo "clone success"
 
 # docker
-cd /home/gilc/projects/$DIRECTORY_REPO
+cd /home/$userName/projects/$DIRECTORY_REPO
 echo $DEVSHELL_PROJECT_ID
 export PROJECT_ID=$DEVSHELL_PROJECT_ID
 
@@ -36,11 +40,11 @@ echo $Tag_Version
 docker tag eu.gcr.io/$PROJECT_ID/$Dbt_project_Name \eu.gcr.io/$PROJECT_ID/$Dbt_project_Name:$Tag_Version
 docker images
 
-echo docker tag $Tag_Version
+echo image docker tag is: $Tag_Version
 
 docker push eu.gcr.io/$PROJECT_ID/$Dbt_project_Name:$Tag_Version
 
-echo push docker $Tag_Version success
+echo push to docker $Tag_Version success.
 
 echo container images describe eu.gcr.io/$PROJECT_ID/$Dbt_project_Name:$Tag_Version
 

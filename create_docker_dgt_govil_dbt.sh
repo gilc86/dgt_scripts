8d05c5f5c74b11994b47448eceb92aaa295336e5
LAPTOP-HAHEDJOL\Gil <gilc86@gmail.com>
2022-12-24 14:52:17 +0200

    add parameter $Dbt_project_Name

--- create_docker_dgt_govil_dbt.sh
+++ create_docker_dgt_govil_dbt.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "create docker for dgt_govil_dbt"
+export Dbt_project_Name=dgt_govil_dbt
+
+echo create docker for $Dbt_project_Name
 echo "creator: Gil Kal"
 
 cd /home/gilc/projects/
@@ -21,7 +23,7 @@ cd /home/gilc/projects/$DIRECTORY_REPO
 echo $DEVSHELL_PROJECT_ID
 export PROJECT_ID=$DEVSHELL_PROJECT_ID
 
-docker build . -f ./dbt/Dockerfile -t eu.gcr.io/$PROJECT_ID/dgt_govil_dbt:latest
+docker build . -f ./dbt/Dockerfile -t eu.gcr.io/$PROJECT_ID/$Dbt_project_Name:latest
 
 echo docker build success from $DIRECTORY_REPO
 
@@ -33,11 +35,20 @@ export Tag_Version=$(git describe --tags --abbrev=0)
 
 echo $Tag_Version
 
-docker tag eu.gcr.io/$PROJECT_ID/dgt_govil_dbt \eu.gcr.io/$PROJECT_ID/dgt_govil_dbt:$Tag_Version
+docker tag eu.gcr.io/$PROJECT_ID/$Dbt_project_Name \eu.gcr.io/$PROJECT_ID/$Dbt_project_Name:$Tag_Version
 docker images
 
 echo docker tag $Tag_Version
 
-docker push eu.gcr.io/$PROJECT_ID/dgt_govil_dbt:$Tag_Version
+docker push eu.gcr.io/$PROJECT_ID/$Dbt_project_Name:$Tag_Version
 
 echo push docker $Tag_Version success
+
+echo container images describe eu.gcr.io/$PROJECT_ID/$Dbt_project_Name:1.0.0
+
+
+
+
+
+
+

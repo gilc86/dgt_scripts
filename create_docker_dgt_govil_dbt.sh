1a5704c9eeb15fcf02c343aaf01f46d89dfdbc35
gilc86 <68189826+gilc86@users.noreply.github.com>
2022-12-26 14:48:52 +0200

    add case between prod and test

--- create_docker_dgt_govil_dbt.sh
+++ create_docker_dgt_govil_dbt.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+export ProjectNameGCP= $(gcloud config get-value project)# Example: dgt-gcp-egov-test-govilbi-0
 export Dbt_project_Name=dgt_govil_dbt
+export Test_ProjectNameGCP=dgt-gcp-egov-test-govilbi-0
+export Prod_ProjectNameGCP=dgt-gcp-egov-prod-govilbi-0
 export userName=$(gcloud config list account --format "value(core.account)")
 export userName=$(cut -d "@" -f1 <<< "$userName")
-export ENVIRONMENT_NAME=composer-dgt-gcp-egov-test-govilbi-2 #compserName
+export test_composer_environmentName=composer-dgt-gcp-egov-test-govilbi-2 #compserName
+export prod_composer_environmentName=composer-dgt-gcp-egov-prod-govilbi-2 #compserName change name!!!????
+export composer_environmentName
 export LOCATION=europe-west3
 echo $userName
 export DIRECTORY_REPO=govil_airflow_k8_dbt
-export gcs_composer=europe-west3-composer-dgt-g-97f74c13-bucket
+
+export test_gcs_composer=europe-west3-composer-dgt-g-97f74c13-bucket
+export prod_gcs_composer=europe #change name!!!????
+export gcs_composer
 export Dag_DBT_Name=dgt_airflow_k8_dbt.py
 
 echo create docker for $Dbt_project_Name
@@ -14,8 +22,21 @@ echo "creator: Gil Kal"
 
 cd /home/$userName/projects/
 
-# Check the $DIRECTORY_REPO is exists and delete
+# Check name $ProjectNameGCP and conig variables Prod or Test
+case $ProjectNameGCP in
+	$Test_ProjectNameGCP) 
+	$composer_environmentName=$test_composer_environmentName
+	$gcs_composer=$test_gcs_composer
+   ;;
+	$Prod_ProjectNameGCP)) 
+	$composer_environmentName=$test_composer_environmentName
+	$gcs_composer=$test_gcs_composer
+   ;;
+esac
+echo The project name $ProjectNameGCP and composer name: $composer_environmentName
 
+
+# Check the $DIRECTORY_REPO is exists and delete Directory
 if [ -d "$DIRECTORY_REPO" ]; then
   echo "$DIRECTORY_REPO does exist."
   rm -rf $DIRECTORY_REPO
@@ -57,13 +78,9 @@ echo container images describe eu.gcr.io/$PROJECT_ID/$Dbt_project_Name:$Tag_Vers
 echo copy Dag file to gcs
 gsutil cp /home/$userName/projects/$DIRECTORY_REPO/dags/$Dag_DBT_Name gs://$gcs_composer/dags/
 #Composer2
-gcloud composer environments update $ENVIRONMENT_NAME \
+gcloud composer environments update $composer_environmentName \
   --location $LOCATION \
   --update-env-variables=DGT_AIRFLOW_DBT_TAG=$Tag_Version
-
-
-
-
 
 
 

f88779cba14044c120525dbf6d25b3c8e49df25c
gilc86 <68189826+gilc86@users.noreply.github.com>
2023-01-23 04:29:18 +0200

    fix bug chmod

--- script_exec/exec_create_docker_dgt_govil_dbt.sh
+++ script_exec/exec_create_docker_dgt_govil_dbt.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 echo " exec create docker for dgt_govil_dbt"
 echo "creator: Gil Kal"
-
+export DIRECTORY_REPO=govil_airflow_k8_dbt
+git clone https://github.com/gilc86/$DIRECTORY_REPO.git
 export userName=$(gcloud config list account --format "value(core.account)")
 export userName=$(cut -d "@" -f1 <<< "$userName")
 echo Name of user: $userName
 
 mkdir -p /home/$userName/projects/script_exec
 cp /home/$userName/projects/dgt_scripts/script_exec/exec_create_docker_dgt_govil_dbt.sh /home/$userName/projects/script_exec
-chmod u+x /home/$userName/projects/script_exec/exec_create_docker_dgt_govil_dbt.sh
+cd /home/$userName/projects/script_exec/
+chmod u+x exec_create_docker_dgt_govil_dbt.sh
+
 cd /home/$userName/projects/
 
 # Check the $DIRECTORY_REPO is exists and delete
@@ -22,8 +25,10 @@ if [ -d "$DIRECTORY_REPO" ]; then
   echo "clone success dgt_scripts"
 fi
 
-cd $DIRECTORY_REPO
+cd /home/$userName/projects/script_exec/
+chmod u+x exec_create_docker_dgt_govil_dbt.sh
 
+cd /home/$userName/projects/$DIRECTORY_REPO
 chmod u+x create_docker_dgt_govil_dbt.sh
 ./create_docker_dgt_govil_dbt.sh
 

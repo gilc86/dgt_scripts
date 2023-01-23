acdabfb8c5158c8fe54999082cf50487aac4b251
gilc86 <68189826+gilc86@users.noreply.github.com>
2023-01-23 05:15:53 +0200

    add field to config

--- create_docker_dgt_govil_dbt.sh
+++ create_docker_dgt_govil_dbt.sh
@@ -74,6 +74,8 @@ echo copy config json Dag file to gcs
 echo $Tag_Version
 export tmp=$(mktemp)
 jq '."Tag_Version" = "'"$Tag_Version"'"' /home/$userName/projects/$DIRECTORY_REPO/dags/$dag_config_name > "$tmp" && mv "$tmp" /home/$userName/projects/$DIRECTORY_REPO/dags/$dag_config_name
+jq '."artifact_registry" = "'"$artifact_registry"'"' /home/$userName/projects/$DIRECTORY_REPO/dags/$dag_config_name > "$tmp" && mv "$tmp" /home/$userName/projects/$DIRECTORY_REPO/dags/$dag_config_name
+jq '."Dbt_project_Name" = "'"$Dbt_project_Name"'"' /home/$userName/projects/$DIRECTORY_REPO/dags/$dag_config_name > "$tmp" && mv "$tmp" /home/$userName/projects/$DIRECTORY_REPO/dags/$dag_config_name
 gsutil cp /home/$userName/projects/$DIRECTORY_REPO/dags/$dag_config_name gs://$gcs_composer/dags/
 
 ################################################################################
